70fa18ae77d85969093aa1950eef54e74cb7c73b
Will Linehan <will@wjltechservices.co.uk>
2020-02-17 16:37:19 +0000

    Initial commit

---
+++ src/main/java/com/wjltechservices/LeisurepassApplication.java
@@ -0,0 +1,13 @@
+package com.wjltechservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeisurepassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeisurepassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjltechservices/LeisurepassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjltechservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeisurepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
